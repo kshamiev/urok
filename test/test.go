9da2d53a133a1329e07af3bc8d792ebaf277ef18
Ilosa <kshamiev@hostkey.ru>
2018-12-11 13:41:54 +0300

    !

--- test/test.go
+++ test/test.go
@@ -12,6 +12,15 @@ func main() {
 	fmt.Printf("formula: %s\nresult: %f", f, result)
 }
 
+const (
+	formula_skopen  = "("
+	formula_skclose = ")"
+	formula_minus   = "-"
+	formula_plus    = "+"
+	formula_um      = "*"
+	formula_del     = "/"
+)
+
 func Formula(f string) (res float64) {
 	f = strings.TrimSpace(f)
 	res, _ = formula(strings.Split(f, " "))
@@ -19,52 +28,57 @@ func Formula(f string) (res float64) {
 }
 
 func formula(data []string) (res float64, level int) {
+	var (
+		err error
+		r   float64
+		n   int
+	)
 	for i := 0; i < len(data); i++ {
 		if len(data) < i+2 {
 			break
 		}
 		switch data[i] {
-		case ")":
+		case formula_skclose:
 			return res, i
-		case "/":
+		case formula_del:
 			i++
-			if data[i] == "(" {
+			if data[i] == formula_skopen {
 				i++
-				r, n := formula(data[i:])
+				r, n = formula(data[i:])
 				res = res / r
 				i = i + n
-			} else if fl, err := strconv.ParseFloat(data[i], 64); err == nil {
-				res = res / fl
+			} else if r, err = strconv.ParseFloat(data[i], 64); err == nil {
+				res = res / r
 			}
-		case "*":
+		case formula_um:
 			i++
-			if data[i] == "(" {
+			if data[i] == formula_skopen {
 				i++
-				r, n := formula(data[i:])
+				r, n = formula(data[i:])
 				res = res / r
 				i = i + n
-			} else if fl, err := strconv.ParseFloat(data[i], 64); err == nil {
-				res = res * fl
+			} else if r, err = strconv.ParseFloat(data[i], 64); err == nil {
+				res = res * r
 			}
-		case "+":
+		case formula_plus:
 			i++
-			if data[i] == "(" {
+			if data[i] == formula_skopen {
 				i++
-				r, n := formula(data[i:])
+				r, n = formula(data[i:])
 				res = res / r
 				i = i + n
-			} else if fl, err := strconv.ParseFloat(data[i], 64); err == nil {
-				res = res + fl
+			} else if r, err := strconv.ParseFloat(data[i], 64); err == nil {
+				res = res + r
 			}
-		case "-":
+		case formula_minus:
 			i++
-			if data[i] == "(" {
+			if data[i] == formula_skopen {
 				i++
-				r, n := formula(data[i:])
+				r, n = formula(data[i:])
 				res = res / r
 				i = i + n
-			} else if fl, err := strconv.ParseFloat(data[i], 64); err == nil {
-				res = res - fl
+			} else if r, err := strconv.ParseFloat(data[i], 64); err == nil {
+				res = res - r
 			}
 		default:
 			if fl, err := strconv.ParseFloat(data[i], 64); err == nil {
