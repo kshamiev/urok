3da81f2e0157104ace665fa069e81644fb3e6df8
Konstantin Shamiev <konstantin@shamiev.ru>
2018-12-14 01:03:48 +0300

    urok vatiable

--- var/rune.go
+++ var/rune.go
@@ -5,27 +5,25 @@ import (
 )
 
 func main() {
-
 	Вася := 5
 	Петя := 10
-
 	Муся := Вася + Петя
 	fmt.Println(Муся)
-	fmt.Println()
-
 	// Строка
 	str := "Привет, Мир! 你好世界"
 	fmt.Println("string: ", str, len(str))
 	for i, v := range str {
 		if v == 'в' {
-			fmt.Println("начинается с байта ", i, " OK")
+			fmt.Println("буква начинается с байта ", i, " OK")
+			fmt.Printf("%v at position %d\n", v, i)
 		} else if v == '世' {
-			fmt.Println("начинается с байта ", i, " OK")
+			fmt.Println("буква начинается с байта ", i, " OK")
+			fmt.Printf("%v at position %d\n", v, i)
+		} else {
+			fmt.Printf("%v at position %d\n", v, i)
 		}
-		fmt.Printf("%v - %#U at position %d\n", v, v, i)
 	}
 	fmt.Println()
-
 	// Байты
 	bin := []byte(str)
 	fmt.Println("binary: ", bin, len(bin))
@@ -33,18 +31,17 @@ func main() {
 		fmt.Printf("raw binary index: %v, oct: %v, hex: %x\n", i, v, v)
 	}
 	fmt.Println()
-
 	// Руны
 	runes := []rune(str)
 	fmt.Println("runes: ", runes, len(runes))
 	for i, v := range runes {
 		if v == 'в' {
 			fmt.Println("начинается с позиции ", i, " OK")
+			fmt.Printf("%v at position %d\n", v, i)
 		} else if v == '世' {
 			fmt.Println("начинается с позиции ", i, " OK")
+			fmt.Printf("%v at position %d\n", v, i)
 		}
-		fmt.Printf("%v - %#U at position %d\n", v, v, i)
 	}
 	fmt.Println()
-
 }
--- var/slice.go
+++ var/slice.go
@@ -10,18 +10,14 @@ var Srez1 = make([]int, 10, 15)              //
 // var Srez2 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9} // рекомендуется
 
 func main() {
-	// Srez1Func := make([]int, 10, 15)
-	// fmt.Println(Srez0, Srez1Func, Srez2)
+	Srez1 = append(Srez1, []int{91, 92, 93, 94, 95, 96, 97, 98, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99}...)
 
-	Srez1 = append(Srez1, []int{91, 92, 93, 94, 95, 96, 97, 98, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99}...)
-	Srez1[3] = 999
 	fmt.Println(len(Srez1), cap(Srez1))
 	sampleSlice(Srez1)
-
+	fmt.Println(len(Srez1), cap(Srez1))
 	fmt.Println(Srez1)
 
 	sampleSliceLink(&Srez1)
-
 	fmt.Println(Srez1)
 
 	// Правильное копирование среза
@@ -33,8 +29,11 @@ func main() {
 
 // передача по значению
 func sampleSlice(sl []int) {
+	sl[3] = 888
 	sl = append(sl, 100)
-	sl[3] = 111
+	sl[4] = 111
+	fmt.Println(len(sl), cap(sl))
+	fmt.Println(sl)
 }
 
 // передача по ссылке
