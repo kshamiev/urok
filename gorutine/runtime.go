66b31e3ad173996747390475446a301c86e6bace
Ilosa <kshamiev@hostkey.ru>
2018-12-15 19:55:49 +0300

    gorutine

--- gorutine/runtime.go
+++ gorutine/runtime.go
@@ -1,22 +1,60 @@
 // примеры работы с горутинами
-// gorutine - облегченный отдельный поток (обычно это функция запускаемая как отедльная программа)
-// gorutine - не зависимо выполняемая логичаская часть программы (обычно функция) запущенная оператором go
+// gorutine - облегченный отдельный поток (это функция или метод запускаемая как отедельная программа)
+// gorutine - не зависимо выполняемая логичаская часть программы запущенная оператором go
 // runtime занимается распределением горутин по тредам
 // go run -race test.go (аргумент race) дает информацию о взаимодействии горутин с общими объектами
-// с его помощью можно тестировать отдельный участки программы работающие с горутинами
+// с его помощью можно тестировать отдельный участки программы работающие с горутинами на предмет дедлоков
 // deadlock это когда мы бесконечно ждем чтения из канала и не можем прочитать или наоборот бесконечно записать в канал
+// range & select без default блокируются пока не получат из канала значение
 package main
 
 import (
 	"fmt"
-	//	"runtime"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
-// Пример конструкции управление работой горутин с помощью стандартного закрытие канала
-func SamplechanelBlock() {
+func main() {
+	Sample()
+	// SamplechanelBlock1()
+	// SamplechanelBlock2()
+	// SampleRuntimeSelect()
+}
+
+func Sample() {
+	fmt.Println("старт")
+	// можно запустить функцию
+	go process1(0)
+	// можно запустить анонимную функцию
+	go func() {
+		fmt.Println("Анонимный запуск")
+	}()
+
+	// Можем запустить много горутин
+	for i := 0; i < 1000; i++ {
+		go process1(i)
+	}
+
+	var ch = make(chan int)
+	close(ch)
+	// num := <-ch
+	// fmt.Println(num, "ТАК ЛУЧШЕ НЕ ДЕЛАТЬ")
+	if num1, ok := <-ch; ok {
+		fmt.Println(num1)
+	} else {
+		fmt.Println("close")
+	}
+	// Нужно дождаться заверешния выполнения
+	fmt.Scanln()
+
+}
+
+func process1(i int) {
+	fmt.Println("обработка: ", i)
+}
+
+// //////////////////////////
+// Пример конструкции управление работой горутин с помощью стандартного закрытия канала
+func SamplechanelBlock1() {
 
 	// буферизированные
 	ch := make(chan int64, 500)
@@ -37,123 +75,43 @@ func SamplechanelBlock() {
 	ch <- 45
 	ch <- 34
 	ch <- 76
+	ch <- 123
 	close(ch)
-
-	// selec с блоком default
-	// case ch1, ok := <-ch:
-	// ...
 	fmt.Scanln()
 }
 
-// пример использование Once & atomic
-// выполенине содержимого только один раз (даже если в разных горутинах)
-func SampleRuntimeOnce() {
+// Пример конструкции управление работой горутин с помощью стандартного закрытия канала
+func SamplechanelBlock2() {
 
-	var onc sync.Once
+	// буферизированные
+	ch := make(chan int64, 500)
 
-	var T = int64(345)
-	for i := 0; i < 10; i++ {
-		go func() {
-			atomic.AddInt64(&T, 10)
-			fmt.Println(atomic.LoadInt64(&T))
-			onc.Do(func() {
-				fmt.Println("sync.Once")
-			})
-		}()
-	}
+	go func() {
+		for {
+			select {
+			// Если есть задача, то ее нужно обработать
+			// Блокируется пока канал не будет закрыт, либо не поступит новая задача
+			case num, ok := <-ch:
+				if ok {
+					time.Sleep(time.Second * 1)
+					fmt.Println("FOR ITER ", num)
+				} else {
+					fmt.Println("CLOSE")
+					return
+				}
+				// ... следующие кейсы
+			}
+		}
+	}()
+	ch <- 45
+	ch <- 34
+	ch <- 76
+	ch <- 123
+	close(ch)
 	fmt.Scanln()
-	fmt.Print("res: ", T)
 }
 
-// Удобно работать с горутинами через структуру
-// задаем каналы в свойства структуры после этого запускаем метод в горутине в конструкторе
-
-/////////////////////////////////////////////////////////////
-// Пример работы Mutex WaitGroup Atomic
-
-type account1Mutex struct {
-	sync.Mutex
-	balance float64
-}
-
-// блокирует для других горутин любую работу с shared обьектом
-// для этого такой блок прописывается во всех необходимых методах
-func (a *account1Mutex) accountCalc() {
-	a.Lock()
-	defer a.Unlock()
-	// здесь какая-то работа
-	// другие горутины ждут (полная блокировка)
-}
-
-type account2RWMutex struct {
-	sync.RWMutex
-	balance float64
-}
-
-// блокировка на чтение другие горутины могут читать
-// блокировка на запись будет ждать прочтения
-func (a *account2RWMutex) balanceLoad() float64 {
-	a.RLock()
-	defer a.RUnlock()
-	return a.balance
-}
-
-// блокировка на запись
-// будет ждать пока все прочитают
-// когда будет изменять все будут ждать
-func (a *account2RWMutex) balanceSet(x float64) {
-	a.Lock()
-	defer a.Unlock()
-	a.balance += x
-}
-
-type atomicCnt struct {
-	val int64
-}
-
-// атомарно будет добавлять значение в горутинах
-func (a *atomicCnt) Add(x int64) {
-	atomic.AddInt64(&a.val, x)
-}
-
-// атомарно будет читать значение в горутинах
-func (a *atomicCnt) Value() int64 {
-	return atomic.LoadInt64(&a.val)
-}
-
-func SampleRuntimeWaitGroupAndMutex() {
-
-	var wg sync.WaitGroup
-
-	// счетчик выполняющийхся задач (горутин) (ставится в контролирующей функции перед запуском горутины)
-	wg.Add(3)
-
-	// ставится в порождаемой горутине в блок завершения ее работы убавляет счетчик на 1 (передается по ссылке)
-	wg.Done()
-	wg.Done()
-	wg.Done()
-
-	// ждет пока счетчик не будет равено 0 (ставится в контролирующей функции после запуска горутин)
-	wg.Wait()
-
-	//////
-
-	acc1 := account1Mutex{}
-	acc1.accountCalc()
-
-	acc2 := account2RWMutex{}
-	acc2.balanceLoad()
-	acc2.balanceSet(34.56)
-	acc2.balanceLoad()
-
-	////
-
-	atm := &atomicCnt{}
-	atm.Add(64)
-
-}
-
-////////////////////////////////////////////////
+// //////////////////////////////////////////////
 
 // пример работы свича select для горутин
 func SampleRuntimeSelect() {
@@ -195,16 +153,14 @@ func SampleRuntimeSelect() {
 			// если блок default указан программа будет проваливаться в него и блокировки ожидания не будет
 			// (плохо поскольку греем космос и тратим ресурсы)
 			default:
-				//				fmt.Println("SELECT DEFAULT")
+				time.Sleep(time.Second * 1)
+				fmt.Println("FOR ITERATION END ", cnt)
+				if cnt >= 30 {
+					controlCH <- true
+					return
+				}
 			}
-			time.Sleep(time.Second * 1)
-			fmt.Println("FOR ITERATION END ", cnt)
-			if cnt >= 30 {
-				controlCH <- true
-				return
-			}
-
-			// использовать в тяжелых горутинах в начале или конце бесконечного цикла
+			// использовать в тяжелых горутинах в начале или конце бесконечного цикла!
 			// говорит runtime что можно прерваться и преключиться на другие задачи
 			// runtime.Gosched()
 		}
@@ -223,122 +179,4 @@ func SampleRuntimeSelect() {
 	} else {
 		fmt.Sprintln("FINISH OK")
 	}
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// особенность буферизированного канала
-// особенность: после закрытия канала из него можно читать.
-// особенность: после закрытия буферизированного канала в нем сохраняются ранее добавленные и не обработанные данные
-func SampleRuntime2() {
-	stuff := make(chan int64, 7)
-	for i := int64(0); i < 19; i = i + 3 {
-		stuff <- i
-	}
-	close(stuff)
-	fmt.Println("Res: ", process(stuff))
-
-}
-
-func process(input <-chan int64) (res int64) {
-	for r := range input {
-		res += r
-	}
-	return
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-// родительская функция верхнего уровня
-// то есть какя-то программа которая использует горутины (или которой нужны горутины) для убыстрения
-func SampleRuntimeKanal2() {
-	chSet := make(chan int64)
-	chGet := make(chan int64)
-	go samplewrapper2(chSet, chGet)
-	for i := int64(1); i < 20; i = i + 3 {
-		chSet <- i
-		// тут что то можно делать паралельно тяжелое
-		res := <-chGet
-		fmt.Println(res)
-	}
-}
-
-// оберточная функция, посредник, реализующая сам механизм взаимодействия с родительской программой
-func samplewrapper2(chIn chan int64, chOut chan int64) {
-	for v := range chIn {
-		chOut <- sampleWork2(v)
-	}
-}
-
-// непосредственно рабочая функция которая работает в отдельной программе
-func sampleWork2(i int64) int64 {
-	return i + 10
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-// родительская функция верхнего уровня
-// то есть какя-то программа которая использует горутины (или которой нужны горутины) для убыстрения
-func SampleRuntimeKanal1() {
-	chSend := make(chan int64)
-	chInput := samplewrapper1(chSend)
-	for i := int64(1); i < 20; i = i + 3 {
-		chSend <- i
-		// тут что то можно делать паралельно тяжелое
-		res := <-chInput
-		fmt.Println(res)
-	}
-}
-
-// оберточная функция посредник реализующая сам механизм взаимодействия с родительской программой
-// принимает и отдает канал, запускает внутри себя горутину используя замыкание
-func samplewrapper1(chIn <-chan int64) <-chan int64 {
-	var chOut = make(chan int64)
-	go func() {
-		for v := range chIn {
-			chOut <- sampleWork1(v)
-		}
-	}()
-	return chOut
-}
-
-// непосредственно рабочая функция которая работает в отдельной программе
-func sampleWork1(i int64) int64 {
-	return i + 10
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// базовый синтаксис и механизм запуска горутин
-func SampleRuntime() {
-	// создание не буфиризированного канала
-	// ch := make(chan int64)
-	// создание буфиризированного канала
-	// ch := make(chan int64, 7)
-
-	// запускаем функцию
-	go sampleCalc(999)
-
-	// запускаем анонимную функцию
-	go func() {
-		fmt.Println("anonim")
-	}()
-
-	// запускаем много функций
-	arr := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	for _, v := range arr {
-		go sampleCalc(v)
-		// захватываемые переменные нужно явно передавать
-		// поскольку в момент выполнения и вывода значения
-		// они могут быть уже изменены родительской программой
-		go func(i int64) {
-			fmt.Println(i)
-		}(v)
-
-	}
-	fmt.Scanln()
-}
-
-func sampleCalc(num int64) {
-	fmt.Println(num)
 }
---
+++ gorutine/shared_data_mutex_wg.go
@@ -0,0 +1,88 @@
+// Go рутины, одновременно работающие с общими данными сами собой не могут синхронизироваться
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+var mu sync.RWMutex
+
+// Пусть у нас есть Счет
+type Account struct {
+	balance float64
+}
+
+func (a *Account) Balance() float64 {
+	// блокировка на чтение
+	// никто не сможет менять данные в исключительной блокировке прока читаем
+	// и не можем прочитать пока не изменят данные и не будет снята исключительная блокирвка
+	mu.RLock()
+	defer mu.RUnlock()
+	return a.balance
+}
+
+func (a *Account) Deposit(amount float64) {
+	// полная блокировка
+	// никто не сможет прочитать (также пройти RLock) пока не изменим данные
+	// также будем ждать пока все не прочитают и не будет снята блокировка на чтение
+	mu.Lock()
+	defer mu.Unlock()
+	log.Printf("depositing: %f", amount)
+	a.balance += amount
+}
+
+func (a *Account) Withdraw(amount float64) {
+	mu.Lock()
+	defer mu.Unlock()
+	if amount > a.balance {
+		return
+	}
+	log.Printf("withdrawing: %f", amount)
+	a.balance -= amount
+}
+
+func main() {
+
+	var controlGoRun = 0
+	var wg sync.WaitGroup
+	wg.Add(1000)
+
+	acc := Account{}
+	// запускаем отдельные программы
+	for i := 0; i < 1000; i++ {
+		go func() {
+			// Каждая из которых, производит операции с аккаунтом
+			for j := 0; j < 100; j++ {
+				// Иногда снимает деньги
+				if j%2 == 1 {
+					acc.Withdraw(50)
+					continue
+				}
+				// иногда кладет
+				acc.Deposit(50)
+			}
+			controlGoRun++
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	// Чтоже получится в результате
+	fmt.Println(acc.Balance(), controlGoRun)
+
+}
+
+//
+func closure() {
+	// Функции захватывают переменные в области видимости
+	// Но, чтобы передавать значение, требуется явно передавать его в функцию
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println("Got", i)
+		}(i)
+	}
+	fmt.Scanln()
+}
