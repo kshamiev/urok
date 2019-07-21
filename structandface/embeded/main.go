48911cca9ba02ba69da1c88c41ff0c5425cd4d12
kshamiev <konstantin@shamiev.ru>
2019-07-21 22:07:27 +0300

    !

---
+++ structandface/embeded/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/token"
+	"io"
+)
+
+func main() {
+	obj := &MyControl{}
+	obj.ID = 67
+	obj.Name = "Popcorn"
+	obj.Price = 56.78
+	obj.Calc()
+	obj.Design()
+	obj.Names()
+	obj.Control.Calc()
+	obj.Control.Design()
+	obj.Control.Names()
+
+	Dumper()
+}
+
+type MyControl struct {
+	ID   uint64
+	Name string
+	Price float64
+	Control
+}
+
+func (ctr MyControl) Names() {
+	fmt.Println("Names", ctr.ID, ctr.Name, ctr.Price)
+	fmt.Println("Names", ctr.Control.ID, ctr.Control.Name, ctr.Control.Price)
+	fmt.Println("Names", ctr.Control.ID, ctr.Control.Name, ctr.Control.Price)
+}
+
+type Control struct {
+	ID   uint64
+	Name string
+	Price float64
+}
+
+func (ctr *Control) Design() {
+	fmt.Println("Design", ctr.ID, ctr.Name, ctr.Price)
+}
+
+func (ctr Control) Calc() {
+	fmt.Println("Calc", ctr.ID, ctr.Name, ctr.Price)
+}
+
+func (ctr Control) Names() {
+	fmt.Println("Names", ctr.ID, ctr.Name, ctr.Price)
+}
+
+////////////////////////
+
+
+// Dump all variables to STDOUT
+func Dumper(idl ...interface{}) string {
+	ret := dump(idl...)
+	fmt.Print(ret.String())
+	return ret.String()
+}
+
+// dump all variables to bytes.Buffer
+func dump(idl ...interface{}) bytes.Buffer {
+	var buf bytes.Buffer
+	var wr io.Writer
+
+	wr = io.MultiWriter(&buf)
+	for _, field := range idl {
+		fset := token.NewFileSet()
+		_ = ast.Fprint(wr, fset, field, ast.NotNilFilter)
+	}
+	return buf
+}
