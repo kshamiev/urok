ed55cbf2988bb43200958a80b3672c67783f0b2e
kshamiev <konstantin@shamiev.ru>
2020-07-25 01:36:36 +0300

    semafor

--- gorutine/semaphore/dev/main.go
+++ gorutine/semaphore/dev/main.go
@@ -2,24 +2,60 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
-	var ch = make(chan int, 5)
-	go func() {
-		time.Sleep(time.Second * 5)
-		for z := range ch {
-			fmt.Println(z)
+	rand.Seed(time.Now().UnixNano())
+	var n = 1000
+	var m = 10000
+	var data = make(map[string][]int64)
+	for i := 0; i < n; i++ {
+		row := make([]int64, 0, m)
+		for j := 0; j < m; j++ {
+			row = append(row, rand.Int63())
 		}
-		fmt.Println("===")
-	}()
+		data[fmt.Sprintf("%d", rand.Int63())] = row
+	}
+	result := semaphore(data, rand.Intn(100))
+	for k, v := range result {
+		fmt.Printf("k:%s v:%d\n", k, v)
+	}
+}
 
-	ch <- 4
-	ch <- 5
-	ch <- 6
-	close(ch)
-	fmt.Println("OK")
-	time.Sleep(time.Second * 10)
-	fmt.Println("OK")
+const maxGR = 4
+
+func semaphore(src map[string][]int64, threads int) map[string]int64 {
+
+	concurentGR := make(chan struct{}, threads)
+	res := make(map[string]int64, len(src))
+
+	for str, arr := range src {
+		concurentGR <- struct{}{}
+
+		go sumArray(str, arr, res)
+
+		<-concurentGR
+	}
+
+	wg.Wait()
+
+	return res
+}
+
+var mu sync.Mutex
+var wg sync.WaitGroup
+
+func sumArray(str string, arr []int64, res map[string]int64) {
+	wg.Add(1)
+	defer wg.Done()
+	r := int64(0)
+	for _, n := range arr {
+		r += n
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	res[str] = r
 }
