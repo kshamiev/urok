04b7b21f68ee02a03092869f249369feb2afedc0
kshamiev <konstantin@shamiev.ru>
2020-11-09 01:40:14 +0300

    feature semaphore

--- pattern/semaphore/semaphore/main.go
+++ pattern/semaphore/semaphore/main.go
@@ -1,3 +1,5 @@
+// Семафор это решение которое контролирует количество процессов - горутин
+// работающих с каким-либо ресурсом или выполняющих какую-то иную работу параллельно.
 package main
 
 import (
@@ -8,6 +10,7 @@ import (
 )
 
 func main() {
+	tStart := time.Now()
 	rand.Seed(time.Now().UnixNano())
 	var n = 1000
 	var m = 10000
@@ -19,36 +22,33 @@ func main() {
 		}
 		data[fmt.Sprintf("%d", rand.Int63())] = row
 	}
-	result := semaphore(data, rand.Intn(100))
-	for k, v := range result {
-		fmt.Printf("k:%s v:%d\n", k, v)
-	}
+	result := semaphore(data, 100)
+	// for k, v := range result {
+	// 	fmt.Printf("k:%s v:%d\n", k, v)
+	// }
+	fmt.Println(len(result))
+	fmt.Println(time.Since(tStart).String())
 }
 
 const maxGR = 4
 
 func semaphore(src map[string][]int64, threads int) map[string]int64 {
-
 	concurentGR := make(chan struct{}, threads)
 	res := make(map[string]int64, len(src))
 
 	for str, arr := range src {
 		concurentGR <- struct{}{}
-
-		go sumArray(str, arr, res)
-
-		<-concurentGR
+		go sumArrayCh(str, arr, res, concurentGR)
 	}
 
 	wg.Wait()
-
 	return res
 }
 
 var mu sync.Mutex
 var wg sync.WaitGroup
 
-func sumArray(str string, arr []int64, res map[string]int64) {
+func sumArrayCh(str string, arr []int64, res map[string]int64, chControl <-chan struct{}) {
 	wg.Add(1)
 	defer wg.Done()
 	r := int64(0)
@@ -58,4 +58,5 @@ func sumArray(str string, arr []int64, res map[string]int64) {
 	mu.Lock()
 	defer mu.Unlock()
 	res[str] = r
+	<-chControl
 }
