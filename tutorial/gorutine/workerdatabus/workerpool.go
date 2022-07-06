8bacd151d361ae3fd6442e5667e764558a1384db
Konstantin Shamiev <konstantin@shamiev.ru>
2022-07-06 20:53:06 +0300

    workerbus

--- tutorial/gorutine/workerdatabus/workerpool.go
+++ tutorial/gorutine/workerdatabus/workerpool.go
@@ -11,7 +11,6 @@ type Task interface {
 type Pool struct {
 	tasks       chan Task      // Канал задач - буф., чтобы родительская программа не блокировалась
 	workerLimit int            // Количество одновременно работающих обработчиков
-	kill        chan struct{}  // Канал отмены, для завершения работы обработчика
 	wg          sync.WaitGroup // WaitGroup для контроля полного завершения работ всех задач и каналов
 }
 
@@ -19,7 +18,6 @@ func NewPool(sizeChanel, workerLimit int) *Pool {
 	p := &Pool{
 		tasks:       make(chan Task, sizeChanel),
 		workerLimit: workerLimit,
-		kill:        make(chan struct{}),
 	}
 	for i := 0; i < p.workerLimit; i++ {
 		p.wg.Add(1)
@@ -46,10 +44,5 @@ func (p *Pool) worker() {
 
 	for task := range p.tasks {
 		task.Execute()
-		select {
-		default:
-		case <-p.kill:
-			return
-		}
 	}
 }
