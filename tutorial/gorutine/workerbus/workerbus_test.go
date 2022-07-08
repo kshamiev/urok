4111ffe982e6a20e0e415f41b050a3cf98745353
Konstantin Shamiev <konstantin@shamiev.ru>
2022-07-08 15:06:17 +0300

    work data bus

--- tutorial/gorutine/workerbus/workerbus_test.go
+++ tutorial/gorutine/workerbus/workerbus_test.go
@@ -8,47 +8,52 @@ import (
 	"github.com/kshamiev/urok/sample/excel/typs"
 )
 
-const count = 1000000
+const (
+	countObject            = 1000000000
+	maxLimitConsumerObject = 1000000
+)
 
-// GOGC=off go test ./tutorial/gorutine/workerbus/. -run=^# -bench=Benchmark_Subscribe -benchtime=1000x -count 5 -cpu 8
+// GOGC=off go test ./tutorial/gorutine/workerbus/. -run=^# -bench=Benchmark_Subscribe -benchtime=100x -count 5 -cpu 8
 func Benchmark_Subscribe(b *testing.B) {
 	b.ReportAllocs()
-	pool := NewWorkerBus(1000, 3)
-
-	// подписчик
-	sub := pool.Subscribe(&typs.Cargo{})
-	go consumer(sub, count-10)
-
-	// отправитель
-	// go func() {
-	for i := 0; i < count; i++ {
-		pool.SendData(&typs.Cargo{Name: fmt.Sprintf("additional_%d", i+1), Amount: 1})
-	}
-	// }()
-
-	// time.Sleep(time.Second)
-
-	pool.Wait()
-}
-
-func Test_Subscribe(t *testing.T) {
-	pool := NewWorkerBus(1000000, 3)
+	pool := NewWorkerBus(maxLimitConsumerObject, 3)
 
 	// подписчики
 	for i := 0; i < 10; i++ {
 		sub := pool.Subscribe(&typs.Cargo{})
-		go consumer(sub, int(GenInt(10000000)))
+		go consumer(sub, int(GenInt(maxLimitConsumerObject)))
 
 	}
 
 	// отправитель
 	go func() {
-		for i := 0; i < count; i++ {
+		for i := 0; i < countObject; i++ {
 			pool.SendData(&typs.Cargo{Name: fmt.Sprintf("additional_%d", i+1), Amount: 1})
 		}
 	}()
 
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second)
+	pool.Wait()
+}
+
+func Test_Subscribe(t *testing.T) {
+	pool := NewWorkerBus(maxLimitConsumerObject, 3)
+
+	// подписчики
+	for i := 0; i < 10; i++ {
+		sub := pool.Subscribe(&typs.Cargo{})
+		go consumer(sub, int(GenInt(maxLimitConsumerObject)))
+
+	}
+
+	// отправитель
+	go func() {
+		for i := 0; i < countObject; i++ {
+			pool.SendData(&typs.Cargo{Name: fmt.Sprintf("additional_%d", i+1), Amount: 1})
+		}
+	}()
+
+	time.Sleep(time.Second)
 	pool.Wait()
 }
 
