d5127b1af9176f83efd4e3ddc32485734143fa0c
Konstantin Shamiev <konstantin@shamiev.ru>
2022-08-06 15:50:27 +0300

    feature manticore start

--- database/manticore/README.md
+++ database/manticore/README.md
@@ -1,4 +1,6 @@
-### Manticore
+## Manticore
+
+## install Docker
 
 ```dockerfile
 docker run --name manticore \
@@ -13,9 +15,14 @@ docker run --name manticore \
 docker run --name manticore \
     -v $(pwd)/etc/manticore.conf:/etc/manticoresearch/manticore.conf \
 	-v $(pwd)/tmp:/var/lib/manticore \
-	-p 192.168.0.101:9308:9308 \
-	-p 192.168.0.101:9312:9312 \
+	-p 9308:9308 \
+	-p 9312:9312 \
 	-d manticoresearch/manticore
 
+docker exec -it manticore bash
+
 gosu manticore indexer --all --rotate
+searchd --status
 ```
+
+## install System
--- database/manticore/app/sample1/main.go
+++ database/manticore/app/sample1/main.go
@@ -24,24 +24,28 @@ import (
 
 func main() {
 	cl := manticore.NewClient()
-	// cl.SetServer("192.168.0.101", 9312)
-	cl.SetServer("192.168.0.101", 9308)
+	// cl.SetServer("192.168.0.101", 9308)
+	cl.SetServer("127.0.0.1", 9308)
 	if _, err := cl.Open(); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	res, err := cl.Sphinxql(`replace into usersidxrt values(1, 'my subject', 'my content')`)
+	fmt.Println("\nREAL TIME INDEX\n")
+
+	res, err := cl.Sphinxql(`replace into usersidxrt values(1005, 'my subject', 'my content')`)
 	fmt.Println(res, err)
-	res, err = cl.Sphinxql(`replace into usersidxrt values(2,'another subject', 'more content')`)
+	res, err = cl.Sphinxql(`replace into usersidxrt values(1002,'another subject', 'more content')`)
 	fmt.Println(res, err)
-	res, err = cl.Sphinxql(`replace into usersidxrt values(5,'again subject', 'one more content')`)
+	res, err = cl.Sphinxql(`replace into usersidxrt values(1003,'again subject', 'one more content')`)
+	fmt.Println(res, err)
+	res, err = cl.Sphinxql(`replace into usersidxrt values(1004,'TEST RIDGEMONT', 'OPIS RIDGEMONT ONE')`)
 	fmt.Println(res, err)
 	res2, err2 := cl.Query("more|another", "usersidxrt")
 	fmt.Println(res2, err2)
 
-	fmt.Println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQq")
+	fmt.Println("\nPLAIN INDEX\n")
 
-	res2, err2 = cl.Query("RIDGEMONT", "usersidx")
+	res2, err2 = cl.Query("RIDGEMONT", "usersidxrt usersidx")
 	fmt.Println(res2, err2)
 }
