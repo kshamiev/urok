6739e7d5f306c70178462ad78bede7f78649655a
Konstantin Shamiev <konstantin@shamiev.ru>
2022-08-10 14:04:09 +0300

    feature manticore start

--- database/dbeaver/postgres/Scripts/Script.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO films
-(title, description, category_id, release_year, price)
-SELECT title, description, category_id, release_year, price
-FROM films WHERE id IN (349, 696);
--- database/manticore/host/main.go
+++ database/manticore/host/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
 
 	"github.com/manticoresoftware/go-sdk/manticore"
 )
@@ -16,39 +14,6 @@ func main() {
 		return
 	}
 
-	fmt.Println("\nREAL TIME INDEX\n")
-	tmunix := int(time.Now().Unix())
-	fmt.Println(tmunix)
-	tm := strconv.Itoa(tmunix)
-	index := "forumrt"
-	// tm := time.Now().String()
-	// tm = "2006-01-02T15:04:05Z07:00"
-	q := `replace into ` + index + ` values(1, 'my subject', 'my content', 9, 9, 1000, '` + tm + `', 1, 34.67, 'str', '{}')`
-	res, err := cl.Sphinxql(q)
-	fmt.Println(res, err)
-	q = `replace into ` + index + ` values(1, 'my subject', 'my content', 8, 8, 1000, '` + tm + `', 1, 34.67, 'str', '{}')`
-	res, err = cl.Sphinxql(q)
-	fmt.Println(res, err)
-	q = `replace into ` + index + ` values(2,'another subject', 'more content', 8, 8, 1000, '` + tm + `', 1, 34.67, 'str', '{}')`
-	res, err = cl.Sphinxql(q)
-	fmt.Println(res, err)
-	q = `replace into ` + index + ` values(3,'again subject', 'one more content', 8, 8, 1000, '` + tm + `', 1, 34.67, 'str', '{}')`
-	res, err = cl.Sphinxql(q)
-	fmt.Println(res, err)
-	q = `replace into ` + index + ` values(4,'TEST RIDGEMONT', 'OPIS RIDGEMONT ONE', 8, 8, 1000, '` + tm + `', 1, 34.67, 'str', '{}')`
-	res, err = cl.Sphinxql(q)
-	fmt.Println(res, err)
-
-	res2, err2 := cl.Query("more", index)
-	fmt.Println(res2, err2)
-
-	fmt.Println("\nPLAIN INDEX\n")
-
 	res3, err3 := cl.Query("PRIDE")
 	fmt.Println(res3, err3)
-
-	qq := manticore.NewSearch("PRIDE", "testidx", "")
-	res4, err4 := cl.RunQuery(qq)
-	fmt.Println(res4, err4)
-
 }
