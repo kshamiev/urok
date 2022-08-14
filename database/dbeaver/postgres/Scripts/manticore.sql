320c996dc3f6fc4fe4de8cd1444b83faf6e68f30
Konstantin Shamiev <konstantin@shamiev.ru>
2022-08-14 14:36:39 +0300

    feature manticore start

--- database/dbeaver/postgres/Scripts/manticore.sql
+++ database/dbeaver/postgres/Scripts/manticore.sql
@@ -7,23 +7,20 @@ INSERT INTO films
 	(title, description, category_id, release_year, price, created_at, updated_at, deleted_at)
 SELECT title, description, category_id, release_year, price, created_at, updated_at, deleted_at FROM films;
 
-INSERT INTO filmss 
+INSERT INTO documents  
 	(title, description, category_id, release_year, price, created_at, updated_at, deleted_at)
-SELECT title, description, category_id, release_year, price, created_at, updated_at, deleted_at FROM films ORDER BY updated_at DESC LIMIT 1000000;
+SELECT title, description, category_id, release_year, price, created_at, updated_at, deleted_at FROM documents1 ORDER BY updated_at DESC LIMIT 1000000;
 
-SELECT count(*) FROM films;
-SELECT count(*) FROM filmss;
+SELECT count(*) FROM documents;
+SELECT count(*) FROM documents1;
 
-UPDATE films SET created_at = now() - INTERVAL '3 MONTH', updated_at = now() - INTERVAL '2 MONTH' WHERE id < 1001;
-UPDATE films SET created_at = now() - INTERVAL '5 MONTH', updated_at = now() - INTERVAL '4 MONTH' WHERE id > 1000;
+UPDATE documents SET created_at = now() - INTERVAL '3 MONTH', updated_at = now() - INTERVAL '2 MONTH' WHERE id < 1001;
+UPDATE documents SET created_at = now() - INTERVAL '5 MONTH', updated_at = now() - INTERVAL '4 MONTH' WHERE id > 1000;
 
-SELECT updated_at, to_timestamp(extract(epoch from updated_at::timestamptz)) FROM manticore_breaker;
+SELECT start_at, to_timestamp(extract(epoch from start_at::timestamptz)) FROM manticore_breaker;
 
-UPDATE films SET created_at = , updated_at = WHERE id BETWEEN 1 AND 10;
-SELECT count(*) FROM films f WHERE created_at BETWEEN '2019-05-10 12:49:34.779' AND '2021-01-10 12:49:34.779';  
+DELETE FROM documents WHERE id > 10;
 
-SELECT * FROM films WHERE 1 = 1 ORDER BY updated_at DESC LIMIT 10;
-SELECT * FROM filmss WHERE 1 = 1 ORDER BY updated_at DESC LIMIT 10;
-DELETE FROM filmss WHERE id > 10;
-
+UPDATE documents SET id = id - 1;
+UPDATE documents SET updated_at = now(), deleted_at = NULL WHERE id > 4;
 
