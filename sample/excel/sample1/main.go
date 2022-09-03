bede02c5b89a611932e62a8604a6fbe50ff1fd79
Konstantin Shamiev <konstantin@shamiev.ru>
2022-09-03 19:41:28 +0300

    feature excel

---
+++ sample/excel/sample1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+
+	"github.com/kshamiev/urok/sample/excel/docexcel"
+	"github.com/kshamiev/urok/sample/excel/typs"
+)
+
+func main() {
+	_, filePath, _, _ := runtime.Caller(0)
+	filePath = filepath.Dir(filePath)
+
+	data := []typs.InvoiceTC{
+		typs.NewInvoiceTC("2345"),
+		typs.NewInvoiceTC("6578"),
+		typs.NewInvoiceTC("83464"),
+	}
+	doc := docexcel.NewSample(filePath + "/combined.xlsx")
+	fp, err := doc.Compile(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := fp.SaveAs(filePath + "/test.xlsx"); err != nil {
+		log.Fatal(err)
+	}
+}
