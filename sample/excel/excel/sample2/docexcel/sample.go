e7d69de5458c742f5898168f7359087eacd1852b
Konstantin Shamiev <konstantin@shamiev.ru>
2022-09-03 20:50:31 +0300

    feature excel

--- sample/excel/excel/sample2/docexcel/sample.go
+++ sample/excel/excel/sample2/docexcel/sample.go
@@ -19,8 +19,27 @@ func (doc Sample) Compile(data []typs.InvoiceTC) (*excelize.File, error) {
 		bu.DeleteStartSheet()
 	}()
 
-	b := bu.NewSheet("test")
-	b.CellRow("B", 2, "E", 2).Value("Заголовок")
+	for i := range data {
+		b := bu.NewSheet(data[i].Number)
+		b.Row += 2
+		b.CellRow("C", b.Row, "D", b.Row+2).Width(25).Value("Name")
+		b.CellRow("E", b.Row, "F", b.Row+2).Value("Length")
+		b.CellRow("G", b.Row, "H", b.Row+2).Value("Width")
+		b.CellRow("I", b.Row, "J", b.Row+2).Value("Height")
+		b.CellRow("K", b.Row, "L", b.Row+2).Value("Amount")
+		b.CellRow("M", b.Row, "N", b.Row+2).Value("Summ")
+		b.Row += 3
+		for _, elm := range data[i].Cargos {
+			b.CellRow("C", b.Row, "D", b.Row+1).Value(elm.Name)
+			b.CellRow("E", b.Row, "F", b.Row+1).Value(elm.Length)
+			b.CellRow("G", b.Row, "H", b.Row+1).Value(elm.Width)
+			b.CellRow("I", b.Row, "J", b.Row+1).Value(elm.Height)
+			b.CellRow("K", b.Row, "L", b.Row+1).Value(elm.Amount)
+			b.CellRow("M", b.Row, "N", b.Row+1).Value(elm.Summ)
+			b.Row += 2
+		}
+
+	}
 
 	return bu.GetFp(), nil
 }
