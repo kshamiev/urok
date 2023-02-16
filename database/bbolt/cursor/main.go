5f02d102e522134769067c9a2b8ffb94461af191
Konstantin Shamiev <konstantin@shamiev.ru>
2023-02-16 17:20:21 +0300

    database bolt add

--- database/bbolt/cursor/main.go
+++ database/bbolt/cursor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -27,20 +28,43 @@ func main() {
 
 // Cursor Итерация данных с помощью курсора
 func Cursor(db *bolt.DB) error {
-	return db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MyBucket"))
+	return db.Update(func(tx *bolt.Tx) error {
 
+		// Создание и удаление "таблицы"
+		b, err := tx.CreateBucketIfNotExists([]byte("MyBucket"))
+		// b, err := tx.CreateBucket([]byte("MyBucket"))
+		if err != nil {
+			return err
+		}
+		// defer tx.DeleteBucket([]byte("MyBucket"))
+		b = tx.Bucket([]byte("MyBucket"))
+
+		// Полный перебор всех значений по порядку
+		// variant 1
 		c := b.Cursor()
 		for k, v := c.First(); k != nil; k, v = c.Next() {
 			fmt.Printf("key=%d, value=%s\n", btoi(k), v)
 		}
-
-		err := b.ForEach(func(k, v []byte) error {
+		// variant 2
+		err = b.ForEach(func(k, v []byte) error {
 			fmt.Printf("key=%d, value=%s\n", btoi(k), v)
 			return nil
 		})
 		if err != nil {
 			return err
+		}
+
+		// Поиск и получение значений по префиксу ключа
+		prefix := []byte("1234")
+		for k, v := c.Seek(prefix); k != nil && bytes.HasPrefix(k, prefix); k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+		}
+
+		// Поиск и получение значений по диапазону ключей
+		min := []byte("1990-01-01T00:00:00Z")
+		max := []byte("2000-01-01T00:00:00Z")
+		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
+			fmt.Printf("%s: %s\n", k, v)
 		}
 
 		return nil
