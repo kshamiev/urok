522759f5a68ceb8885686b6846010491cd357e72
Konstantin Shamiev <konstantin@shamiev.ru>
2023-04-12 18:27:40 +0300

    feature conveyor

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -3,63 +3,18 @@ package main
 // https://stackoverflow.com/questions/71418671/restart-or-shutdown-golang-apps-programmatically
 
 import (
-	"log"
-	"os"
-	"os/exec"
-	"runtime"
-	"syscall"
-	"time"
+	"fmt"
 
-	"github.com/radovskyb/watcher"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
-	log.Println("APP START OR RESTART FROM CHANGE BINARY")
-	err := restart()
-	if err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(time.Hour * 1)
-}
-
-func restart() error {
-	self, err := os.Executable()
-	if err != nil {
-		return err
-	}
-	t_watch := watcher.New()
-	t_watch.FilterOps(watcher.Write)
-	err = t_watch.Add(self)
-	if err != nil {
-		return err
-	}
-	go func() {
-		var err error
-		for {
-			_ = <-t_watch.Event
-			log.Println("RESTART")
-			args := os.Args
-			env := os.Environ()
-			// Windows does not support exec syscall.
-			if runtime.GOOS == "windows" {
-				cmd := exec.Command(self, args[1:]...)
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Stdin = os.Stdin
-				cmd.Env = env
-				err = cmd.Run()
-				if err == nil {
-					os.Exit(0)
-				}
-			} else {
-				err = syscall.Exec(self, args, env)
-			}
-			log.Println(err)
-			// TODO logging error
-		}
-	}()
-	go func() {
-		_ = t_watch.Start(time.Second * 1)
-	}()
-	return nil
+	qu := "select * from articles where avtor_id IN (?) and avtor_id IN (?) and status = true"
+	args1 := []int64{1, 2, 3, 4, 5}
+	args2 := []int64{11, 12, 13, 14, 15}
+	fmt.Println(sqlx.In(qu, args1, args2))
+	// output:
+	// select * from articles where avtor_id IN (?, ?, ?, ?, ?) and avtor_id IN (?, ?, ?, ?, ?) and status = true
+	// [1 2 3 4 5 11 12 13 14 15] <nil>
+	// <nil>
 }
