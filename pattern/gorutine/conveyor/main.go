87f06f8816f6f852b8079d40b08d3a541cb50a65
Konstantin Shamiev <konstantin@shamiev.ru>
2023-04-12 16:26:18 +0300

    feature conveyor

---
+++ pattern/gorutine/conveyor/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	in := make(chan int)
+	out := work(square(in))
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for val := range out {
+			fmt.Println("ответ конвейера: ", val)
+		}
+		fmt.Println("Концерт окончен")
+		wg.Done()
+	}()
+
+	fmt.Println("Введите целое число либо слово exit (ctrl+D) для выхода")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text() // получаем строку
+		if line == "exit" {
+			break
+		} else {
+			number, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			in <- number
+		}
+	}
+	close(in)
+	wg.Wait()
+}
+
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var val int
+		for val = range in {
+			out <- val * val
+		}
+		close(out)
+	}()
+	return out
+}
+
+func work(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var val int
+		for val = range in {
+			out <- val * 2
+		}
+		close(out)
+	}()
+	return out
+}
--- sample/files/sum/main.go
+++ sample/files/sum/main.go
@@ -8,21 +8,45 @@ import (
 	"io"
 	"log"
 	"os"
+	"path"
 )
 
 func main() {
+	// проверка FS
+	const p = "data/session/test.txt"
+	err := os.MkdirAll(path.Dir(p), 0o700)
+	if err != nil {
+		panic(err)
+	}
+	if err = os.WriteFile(p, []byte("test"), 0o600); err != nil {
+		panic(err)
+	}
+	if err = os.Remove(p); err != nil {
+		panic(err)
+	}
+
+	// открываем файл
 	f, err := os.Open("sample/files/sum/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
+	// пример сохранения в другой файл
+	fp, err := os.OpenFile("sample/files/sum/input_new.txt", os.O_CREATE|os.O_WRONLY, 0o600)
+	if _, err = io.Copy(fp, f); err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	// пример контрольной суммы 1
 	h := sha256.New()
 	if _, err := io.Copy(h, f); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%x\n", h.Sum(nil))
 
+	// пример контрольной суммы 2
 	buf := &bytes.Buffer{}
 	if _, err := io.Copy(buf, f); err != nil {
 		log.Fatal(err)
