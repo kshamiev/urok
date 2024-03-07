e97f9e54a185018cae426d2cc2e3889e2d8368b5
Konstantin Shamiev <konstantin@shamiev.ru>
2024-03-07 12:53:31 +0300

    !

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"strings"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/text/encoding/charmap"
 )
 
-func getGOMAXPROCS() int {
-	return runtime.GOMAXPROCS(0)
-}
-
 func main() {
-	fmt.Printf("GOMAXPROCS: %d\n", getGOMAXPROCS())
+	// Запись строки в кодировке Windows-1252
+	encoder := charmap.Windows1251.NewEncoder()
+	s, e := encoder.String("Распоряжения")
+	if e != nil {
+		log.Fatal(e)
+	}
+	_ = os.WriteFile("cmd/dev/example.txt", []byte(s), os.ModePerm)
 
-	in := "qwertyasdfghj"
-
-	fmt.Println(strings.Index(in, ""))
-
+	// Декодировка в UTF-8
+	f, e := os.Open("cmd/dev/example.txt")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	decoder := charmap.Windows1251.NewDecoder()
+	reader := decoder.Reader(f)
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
 }
