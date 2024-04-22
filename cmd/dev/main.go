357591aabcfaeb376cee240ee9aa8cb595ee9ea4
Konstantin Shamiev <konstantin@shamiev.ru>
2024-04-22 21:48:57 +0300

    feature

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,31 +1,90 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
 	"os"
-
-	"golang.org/x/text/encoding/charmap"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	// Запись строки в кодировке Windows-1252
-	encoder := charmap.Windows1251.NewEncoder()
-	s, e := encoder.String("Распоряжения")
-	if e != nil {
-		log.Fatal(e)
-	}
-	_ = os.WriteFile("cmd/dev/example.txt", []byte(s), os.ModePerm)
+	s := &HttpServer{}
+	s.start()
+	lock(nil)
+	s.stop()
+}
 
-	// Декодировка в UTF-8
-	f, e := os.ReadFile("cmd/dev/example.txt")
-	if e != nil {
-		log.Fatal(e)
+type HttpServer struct {
+	server    *http.Server  // Сервер HTTP
+	chControl chan struct{} // Управление ожиданием завершения работы сервера
+	lis       net.Listener
+}
+
+func (rec *HttpServer) start() {
+	var err error
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /task/{id}/", home)
+	rec.server = &http.Server{
+		Addr:           "localhost:8080",
+		Handler:        mux,
+		ReadTimeout:    time.Minute,
+		WriteTimeout:   time.Minute,
+		IdleTimeout:    time.Minute,
+		MaxHeaderBytes: 1048576,
 	}
-	decoder := charmap.Windows1251.NewDecoder()
-	b, e := decoder.Bytes(f)
-	if e != nil {
-		panic(e)
+	if rec.lis, err = net.Listen("tcp", rec.server.Addr); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println(string(b))
+	rec.chControl = make(chan struct{})
+
+	go func() {
+		_ = rec.server.Serve(rec.lis)
+		close(rec.chControl)
+	}()
+}
+
+func (rec *HttpServer) stop() {
+	if rec.lis == nil {
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := rec.server.Shutdown(ctx); err != nil {
+		if err = rec.lis.Close(); err != nil {
+			return
+		}
+	}
+	<-rec.chControl
+	return
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	str := "hello world: " + r.PathValue("id")
+	fmt.Println(str)
+	_, _ = w.Write([]byte(str))
+	w.WriteHeader(http.StatusOK)
+}
+
+// lock run application
+func lock(ch chan os.Signal) {
+	if ch == nil {
+		ch = make(chan os.Signal, 1)
+	}
+	defer func() {
+		ch <- os.Interrupt
+	}()
+	// The correctness of the application is closed by a signal
+	signal.Notify(ch,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+	<-ch
 }
