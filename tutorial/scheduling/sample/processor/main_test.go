d9549d6f37f589d21feb5229a8400a403be26f80
Konstantin Shamiev <p-shamiev@tn.ru>
2024-08-14 21:05:34 +0300

    feature

---
+++ tutorial/scheduling/sample/processor/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"runtime"
+	"testing"
+)
+
+// GOGC=off go test ./tutorial/scheduling/sample/processor/. -run=^# -bench=.+ -benchtime=10x -count 1 -cpu 1
+// GOGC=off go test ./tutorial/scheduling/sample/processor/. -run=^# -bench=.+ -benchtime=10x -count 1 -cpu 16
+
+var numbers = generateList(1e9)
+
+func BenchmarkSequential(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		add(numbers)
+	}
+}
+
+func BenchmarkConcurrentOrParallel(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		addConcurrentOrParallel(runtime.NumCPU(), numbers)
+	}
+}
