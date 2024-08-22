273e50164e271fd597367dcc95b07eaa3d414fab
Konstantin Shamiev <p-shamiev@tn.ru>
2024-08-22 17:33:28 +0300

    !

---
+++ tutorial/gc/ballast_empty.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"time"
+)
+
+// ps -eo pmem,comm,pid,maj_flt,min_flt,rss,vsz --sort -rss | numfmt --header --from-unit=1024 --to=iec --field 6-7 | column -t | egrep "ballast.*"
+// VSZ выделенная виртуальная память
+// RSS выделенная физическая память
+func main() {
+	// блокировка (процессоры отдыхают)
+	<-time.After(time.Hour)
+}
