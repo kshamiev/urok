bcca62ab0fda58fe008c806789e16fbfb4a39b2b
Konstantin Shamiev <p-shamiev@tn.ru>
2024-08-22 17:44:28 +0300

    !

--- tutorial/gc/ballast.go
+++ tutorial/gc/ballast.go
@@ -6,7 +6,7 @@ import (
 
 var ballast []byte
 
-// ps -eo pmem,comm,pid,maj_flt,min_flt,rss,vsz --sort -rss | numfmt --header --from-unit=1024 --to=iec --field 6-7 | column -t | egrep "ballast.*"
+// ps -eo pmem,comm,pid,maj_flt,min_flt,rss,vsz --sort -rss | numfmt --header --from-unit=1024 --to=iec --field 6-7 | column -t | egrep "ballast.*|PID"
 // VSZ выделенная виртуальная память
 // RSS выделенная физическая память
 func main() {
--- tutorial/gc/ballast_empty.go
+++ tutorial/gc/ballast_empty.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-// ps -eo pmem,comm,pid,maj_flt,min_flt,rss,vsz --sort -rss | numfmt --header --from-unit=1024 --to=iec --field 6-7 | column -t | egrep "ballast.*"
+// ps -eo pmem,comm,pid,maj_flt,min_flt,rss,vsz --sort -rss | numfmt --header --from-unit=1024 --to=iec --field 6-7 | column -t | egrep "ballast.*|PID"
 // VSZ выделенная виртуальная память
 // RSS выделенная физическая память
 func main() {
