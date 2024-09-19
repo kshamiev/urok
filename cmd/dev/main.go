44e42633e5d62c93416b6b94a4b03498ea151042
Konstantin Shamiev <p-shamiev@tn.ru>
2024-09-19 18:52:26 +0300

    !

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -5,11 +5,36 @@ import (
 )
 
 func main() {
+	done := make(chan struct{})
+	defer close(done)
 
-	m := min(25, 56)
-	fmt.Println(m)
+	// Генерирует канал, отправляющий целые числа от 0 до 9
+	range10 := rangeChannel(done, 10)
 
-	m = max(25, 56)
-	fmt.Println(m)
+	for num := range takeFirstN(done, range10, 5) {
+		fmt.Println(num)
+	}
+}
+func takeFirstN(ctx context.Context, dataSource <-chan interface{}, n int) <-chan interface{} {
+	// 1
+	takeChannel := make(chan interface{})
 
+	// 2
+	go func() {
+		defer close(takeChannel)
+
+		// 3
+		for i := 0; i < n; i++ {
+			select {
+			case val, ok := <-dataSource:
+				if !ok {
+					return
+				}
+				takeChannel <- val
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return takeChannel
 }
--- pattern/gorutine/producerconsumer/main.go
+++ pattern/gorutine/producerconsumer/main.go
@@ -6,25 +6,30 @@ import (
 	"fmt"
 )
 
-var done = make(chan bool)
-var msgs = make(chan int)
-
-func produce() {
-	for i := 0; i < 10; i++ {
-		msgs <- i
-	}
-	done <- true
+func produceGo() <-chan int {
+	msgCH := make(chan int, 10)
+	go func() {
+		for i := 0; i < 10; i++ {
+			msgCH <- i
+		}
+		close(msgCH)
+	}()
+	return msgCH
 }
 
-func consume() {
-	for {
-		msg := <-msgs
-		fmt.Println(msg)
-	}
+func consumeGo(ch <-chan int) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		for msg := range ch {
+			fmt.Println(msg)
+		}
+		done <- struct{}{}
+	}()
+	return done
 }
 
 func main() {
-	go produce()
-	go consume()
+	msgCH := produceGo()
+	done := consumeGo(msgCH)
 	<-done
 }
